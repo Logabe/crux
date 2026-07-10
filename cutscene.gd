72784c95ee7984bfe76ae2bef3c89cd344f942a8
Logan Bentley <logangbentley@gmail.com>
2026-07-10 21:07:17 +1000

    very janky stuff

---
+++ cutscene.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@export var b1: Button
+@export var b2: Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Label.visible_characters = 0
+	var tween = create_tween()
+	tween.tween_property($Label, "visible_characters", len($Label.text), len($Label.text) * 0.05)
+	await tween.finished
+	await get_tree().create_timer(1).timeout
+	b1.visible = true
+	await get_tree().create_timer(1).timeout
+	b2.visible = true
+	
