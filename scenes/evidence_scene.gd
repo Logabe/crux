8e806c8bc298248d9578c07944d5b691caacd0a1
Logan Bentley <logangbentley@gmail.com>
2026-07-11 12:03:08 +1000

    Add scene outro whoops i forgot this one

--- scenes/evidence_scene.gd
+++ scenes/evidence_scene.gd
@@ -3,5 +3,8 @@ extends Control
 func _ready() -> void:
 	$Label.text = "Scene " + str(Globals.scene_display_number()) + "/" + str(len(Globals.evidence_scenes))
 	$AnimationPlayer.play("intro")
+
 func next():
+	$AnimationPlayer.play("outro")
+	await $AnimationPlayer.animation_finished
 	Globals.go_to_next()
