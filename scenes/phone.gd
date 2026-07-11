026b87c2ed49e4d4b2f67d315f1842d57824393e
Vai <144768865+viywolf@users.noreply.github.com>
2026-07-11 23:20:20 +1200

    Ontik

--- scenes/phone.gd
+++ scenes/phone.gd
@@ -3,12 +3,22 @@ extends TextureButton
 @export var object_id: String = "phone"
 @export var zoom_texture: Texture2D
 
+@onready var rng : RandomNumberGenerator = RandomNumberGenerator.new()
+
 func _ready() -> void:
 	toggle_mode = true
 	toggled.connect(_on_toggled)
 
 func _on_toggled(is_pressed: bool) -> void:
-	if is_pressed:
+	# 20% chance to trigger
+	if is_pressed and (rng.randi_range(0, 5) == 1):
+		var video_node = VideoStreamPlayer.new()
+		video_node.stream = load("res://assets/ontik/fun_video.ogv")
+		add_child(video_node)
+		video_node.play()
+		await video_node.finished
+		video_node.queue_free()
+	elif is_pressed:
 		ClickTracker.register_click(object_id)
 		ZoomPopup.show_zoom(zoom_texture, self)
 	else:
