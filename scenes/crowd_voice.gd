a9dfa35324a92a96df9745641b7c3139b900fddd
sm02-08 <shelleyma02@gmail.com>
2026-07-11 19:34:18 +1000

    aaaaaaaaaaaa
    aaaaaaaaaaaaaaaaaaaaa

--- scenes/crowd_voice.gd
+++ scenes/crowd_voice.gd
@@ -1,9 +1,15 @@
 extends TextureButton
 
+@export var object_id: String = "crowdvoice"
+@export var zoom_texture: Texture2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	toggle_mode = true
+	toggled.connect(_on_toggled)
+
+func _on_toggled(is_pressed: bool) -> void:
+	if is_pressed:
+		ClickTracker.register_click(object_id)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
